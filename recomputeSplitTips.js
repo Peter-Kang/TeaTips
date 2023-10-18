467d0ed689118e5cd19b5edab0d5b1ad8ae7704d
Peter Kang <pxk7062@rit.edu>
2023-10-17 22:38:44 -0400

    Reworked tram-lite componets and layout

--- recomputeSplitTips.js
+++ recomputeSplitTips.js
@@ -5,7 +5,7 @@ function recomputeSplitTips() {
     .querySelector("people-hours")
     .shadowRoot.querySelectorAll("hours-row")
     .forEach((h) => {
-      totalHours += parseFloat(h.getAttribute("hours")) || 0
+      totalHours += parseFloat(h.getAttribute("hours")) || 0;
     });
   if (!isNaN(totalHours) && !isNaN(totalTips) && totalHours > 0) {
     const dollarPerHour = parseFloat(totalTips) / parseFloat(totalHours);
@@ -13,20 +13,18 @@ function recomputeSplitTips() {
       .querySelector("people-hours")
       .shadowRoot.querySelectorAll("hours-row")
       .forEach((h) => {
-        if (h.shadowRoot.getElementById("hours")) {
-          const parsedHours = parseFloat(
-            h.shadowRoot.getElementById("hours").value
-          );
-          if (!isNaN(parsedHours)) {
+        if (h.getAttribute("hours")) {
+          const parsedHours = parseFloat(h.getAttribute("hours"));
+          if (!isNaN(parsedHours) && parsedHours != 0) {
             const display = h.shadowRoot.getElementById("displaySplit");
             const personsTips = (parsedHours * dollarPerHour).toFixed(2);
             const displayText = "$" + personsTips.toString();
             display.innerHTML = displayText;
-          }
-          else
-          {
+          } else {
             h.shadowRoot.getElementById("displaySplit").innerHTML = "$--.--";
           }
+        } else {
+          h.shadowRoot.getElementById("displaySplit").innerHTML = "$--.--";
         }
       });
   }
