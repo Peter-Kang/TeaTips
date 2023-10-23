b9c0fb0213086477a0e1444af104c93caa2c8738
Peter-Kang <pkang93@gmail.com>
2023-10-22 23:24:13 -0400

    Updated logic for display and tips inpout

--- people-hours/hours-row/hours-row.js
+++ people-hours/hours-row/hours-row.js
@@ -1,4 +1,13 @@
 function remove(removeButton) {
+  removeButton.getRootNode().host.setAttribute("hours", 0);
+
+  const event = new CustomEvent("hours-updated", {
+    //when a event is set to bubble, it only child to parent
+    composed: true,
+    //when you do compose it goes through shadow dom boundaries, typically events only go up to the parent
+  });
+  //this.getRootNode().host.dispatchEvent(event);
+  removeButton.getRootNode().host.dispatchEvent(event);
+
   removeButton.getRootNode().host.remove();
-  recomputeSplitTips();
 }
